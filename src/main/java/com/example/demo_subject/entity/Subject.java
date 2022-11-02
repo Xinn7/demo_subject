1af58f2e6df933cec43cc572474171b1244e8ac7
USER <USER@DESKTOP-RTNDO8F>
2022-11-02 16:15:09 +0800

    first commit

---
+++ src/main/java/com/example/demo_subject/entity/Subject.java
@@ -0,0 +1,39 @@
+package com.example.demo_subject.entity;
+
+public class Subject {
+
+	private String num;
+	
+	private String name;
+	
+	private String day;
+	
+	public Subject() {
+		
+	}
+
+	public String getNum() {
+		return num;
+	}
+
+	public void setNum(String num) {
+		this.num = num;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+	
+}
